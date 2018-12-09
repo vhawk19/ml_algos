8222fea1d85a45a85e1a08850c4d4aaea32a9205
varunhawk19 <varunhawk19@gmail.com>
2018-12-09 17:31:09 +0530

    modified to a few better practices

--- Regression.cpp
+++ Regression.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
-using namespace std;
-pair<double,double> type_linear(const std::vector<double>& x, const std::vector<double>& y) {
+typedef std::pair<double,double> d_pair;
+//using namespace std;
+d_pair type_linear(const std::vector<double>& x, const std::vector<double>& y) {
     const auto n    = x.size();
     const auto s_x  = std::accumulate(x.begin(), x.end(), 0.0);
     const auto s_y  = std::accumulate(y.begin(), y.end(), 0.0);
@@ -8,7 +9,7 @@ pair<double,double> type_linear(const std::vector<double>& x, const std::vector<
     const auto s_xy = std::inner_product(x.begin(), x.end(), y.begin(), 0.0);
     const auto a    = (n * s_xy - s_x * s_y) / (n * s_xx - s_x * s_x);
     const auto b    = ((s_xx*s_y-s_x*s_xy)*1.0/(n*s_xx-s_x*s_x)*1.0);
-    pair<double,double> temp(a,b);
+    d_pair temp(a,b);
     return temp;
 }
 int main(){
@@ -24,7 +25,7 @@ int main(){
         std::cin>>temp;
         y.push_back(temp);
     }
-    std::pair <double,double> result=type_linear(x,y);
-    std::cout << result.first<<"x +"<<result.second << '\n';
+    d_pair result=type_linear(x,y);
+    std::cout << result.first<<"x + "<<result.second << '\n';
     return 0;
 }
